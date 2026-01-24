31c1be731c7a7f4ea4256be56d731c492dce631d
kapelu <ddaniel_calderon@hotmail.com>
2026-01-24 00:47:27 -0300

    ✔ Update 24-01-2026

---
+++ 02-.js
@@ -0,0 +1,17 @@
+// Programa una función que te devuelva el texto recortado según el número de caracteres indicados.
+// Por ejemplo: miFunción("Hola Mundo", 4) devolverá "Hola"
+
+const recortarTexto = (texto = "", longitud = 0) =>
+    typeof texto === "string" && longitud > 0
+    ? texto.slice(0, longitud)
+    : "Error: datos inválidos";
+
+  recortarTexto("Hola Mundo", 4); // "Hola"
+
+
+/* explicaion:
+slice(0, longitud) corta el texto desde el inicio hasta el número indicado
+El condicional valida:
+    * que texto sea string
+    * que longitud sea mayor a 0
+*/
---
+++ 03-.js
@@ -0,0 +1,20 @@
+// Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter.
+// Por ejemplo: miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal']
+
+const separarTexto = (texto = "", separador = " ") =>
+    typeof texto === "string" && separador
+        ? texto.split(separador)
+        : "Error: datos inválidos";
+
+separarTexto("hola que tal", " "); // ["hola", "que", "tal"]
+
+/* explicaion:
+Usa funcion flecha para definir separarTexto que recibe dos parámetros: texto y separador
+usa operador ternario para validar si texto es una cadena de texto y separador no está vacío
+Si ambas condiciones son verdaderas, retorna un array usando texto.split(separador)
+Si alguna condición es falsa, muestra un mensaje de error en la consola.
+texto.split(separador) divide la cadena de texto en un array usando el separador indicado
+El condicional valida:
+    * que texto sea string usando typeof
+    * que separador no esté vacío (evaluación implícita)
+*/
